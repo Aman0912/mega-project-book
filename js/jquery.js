0d35f4d9a6b3e921708d0fe9337ed17ee47e55c9
Aman0912 <saklain.sa@gmail.com>
2022-12-23 21:34:41 +0600

    first commit

---
+++ js/jquery.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+
+$(".all-list").owlCarousel({
+"items":3,
+"autoPlay":true,
+"itemsTablet":[768,1],
+"itemsMobile":[479,1],
+"itemsDesktopSmall":[979,2],
+
+});
+
+$(".love1").click(function(){
+  $(".love1").toggleClass("color");
+});
+
+$(".love2").click(function(){
+  $(".love2").toggleClass("color");
+});
+
+$(".love3").click(function(){
+  $(".love3").toggleClass("color");
+});
+
+$(".love4").click(function(){
+  $(".love4").toggleClass("color");
+});
+$(".love5").click(function(){
+  $(".love5").toggleClass("color");
+});
+
+$(".love6").click(function(){
+  $(".love6").toggleClass("color");
+});
+$(".love7").click(function(){
+  $(".love7").toggleClass("color");
+});
+
+$(".loveMid1").click(function(){
+$(".loveMid1").toggleClass("loveMid");
+});
+
+$(".loveMid2").click(function(){
+$(".loveMid2").toggleClass("loveMid");
+});
+
+$(".loveMid3").click(function(){
+$(".loveMid3").toggleClass("loveMid");
+});
+$(".loveMid4").click(function(){
+$(".loveMid4").toggleClass("loveMid");
+});
+$(".loveMid5").click(function(){
+$(".loveMid5").toggleClass("loveMid");
+});
+$(".loveMid6").click(function(){
+$(".loveMid6").toggleClass("loveMid");
+});
+$(".loveMid7").click(function(){
+$(".loveMid7").toggleClass("loveMid");
+});
+$(".loveMid8").click(function(){
+$(".loveMid8").toggleClass("loveMid");
+});
+$(".loveMid9").click(function(){
+$(".loveMid9").toggleClass("loveMid");
+});
+$(".loveMid10").click(function(){
+$(".loveMid10").toggleClass("loveMid");
+});
+$(".loveMid11").click(function(){
+$(".loveMid11").toggleClass("loveMid");
+});
+$(".loveMid12").click(function(){
+$(".loveMid12").toggleClass("loveMid");
+});
+
+});
